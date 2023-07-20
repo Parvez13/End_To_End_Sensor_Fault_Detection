65d5ff220d593779e49d45e3ec03f57fb92a1411
Parvez13 <prvzsohail@gmail.com>
2023-07-20 09:20:05 +0530

    Data Ingestion pipeline complete

---
+++ sensor/pipeline/training_pipeline.py
@@ -0,0 +1,70 @@
+from sensor.entity.config_entity import (TrainingPipelineConfig,
+                                        DataIngestionConfig)
+                                        # DataValidationConfig,
+                                        # DataTransformationConfig,
+                                        # ModelTrainerConfig,
+                                        # ModelEvaluationConfig,
+                                        # ModelPusherConfig
+                                        # )
+from sensor.entity.artifact_entity import (DataIngestionArtifact)
+                                        # DataValidationArtifact,
+                                        # DataTransformationArtifact,
+                                        # ModelTrainerArtifact,
+                                        # ModelEvaluationArtifact,
+                                        # ModelPusherArtifact
+                                        # )
+from sensor.exception import SensorException
+from sensor.logger import logging
+import os,sys
+from sensor.components.data_ingestion import DataIngestion
+# from sensor.components.data_validation import DataValidation
+# from sensor.components.data_transformation import DataTransformation
+# from sensor.components.model_trainer import ModelTrainer
+# from sensor.components.model_evaluation import ModelEvaluation
+# from sensor.components.model_pusher import ModelPusher
+
+class TrainingPipeline:
+
+
+    def __init__(self,training_pipleine_config:TrainingPipelineConfig):
+        try:
+            self.training_pipleine_config=training_pipleine_config
+        except Exception as e:
+            raise SensorException(e, sys)
+
+
+    def start_data_ingestion(self)->DataIngestionArtifact:
+        try:
+            data_ingestion_config =DataIngestionConfig(
+                training_pipeline_config=self.training_pipleine_config)
+            
+            data_ingestion = DataIngestion(data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            return data_ingestion_artifact
+        except Exception as e:
+            raise SensorException(e, sys)
+
+
+
+    def start(self,):
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+
+            # data_validation_artifact = self.start_data_validation(
+            #     data_ingestion_artifact=data_ingestion_artifact)
+
+            # data_transformation_artifact = self.start_data_transformation(
+            #     data_validation_artifact=data_validation_artifact
+            # )
+
+            # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            
+
+            # model_eval_artifact = self.start_model_evaluation(data_validation_artifact=data_validation_artifact,
+            #                 data_transformation_artifact=data_transformation_artifact,
+            #                 model_trainer_artifact=model_trainer_artifact)
+
+            # model_pusher_artifact = self.start_model_pusher(data_transformation_artifact=data_transformation_artifact,
+            #                 model_trainer_artifact=model_trainer_artifact)
+        except Exception as e:
+            raise SensorException(e, sys)
--- sensor/utils.py
+++ sensor/utils.py
@@ -19,14 +19,14 @@ def dump_csv_file_to_mongodb_collection(file_path:str,database_name:str,collecti
         raise SensorException(e,sys)
 
 
-# def export_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
-#     try:
-#         df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
-#         if "_id" in df.columns.to_list():
-#             df=df.drop("_id",axis=1)
-#         return df
-#     except Exception as e:
-#         raise SensorException(e,sys)
+def export_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
+    try:
+        df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        if "_id" in df.columns.to_list():
+            df=df.drop("_id",axis=1)
+        return df
+    except Exception as e:
+        raise SensorException(e,sys)
 
 
 # def write_yaml_file(file_path,data:dict):
