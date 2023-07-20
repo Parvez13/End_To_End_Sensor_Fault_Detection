9290caaf11fae8e2200197271ec664d58d66fe1d
Parvez13 <prvzsohail@gmail.com>
2023-07-20 10:25:30 +0530

    Data validation phase complete

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,23 +1,23 @@
 from sensor.entity.config_entity import (TrainingPipelineConfig,
-                                        DataIngestionConfig)
-                                        # DataValidationConfig,
+                                        DataIngestionConfig,
+                                        DataValidationConfig)
                                         # DataTransformationConfig,
                                         # ModelTrainerConfig,
                                         # ModelEvaluationConfig,
                                         # ModelPusherConfig
                                         # )
-from sensor.entity.artifact_entity import (DataIngestionArtifact)
-                                        # DataValidationArtifact,
-                                        # DataTransformationArtifact,
-                                        # ModelTrainerArtifact,
-                                        # ModelEvaluationArtifact,
-                                        # ModelPusherArtifact
-                                        # )
+from sensor.entity.artifact_entity import (DataIngestionArtifact,
+                                        DataValidationArtifact,
+                                        DataTransformationArtifact,
+                                        ModelTrainerArtifact,
+                                        ModelEvaluationArtifact,
+                                        ModelPusherArtifact
+                                        )
 from sensor.exception import SensorException
 from sensor.logger import logging
 import os,sys
 from sensor.components.data_ingestion import DataIngestion
-# from sensor.components.data_validation import DataValidation
+from sensor.components.data_validation import DataValidation
 # from sensor.components.data_transformation import DataTransformation
 # from sensor.components.model_trainer import ModelTrainer
 # from sensor.components.model_evaluation import ModelEvaluation
@@ -43,6 +43,17 @@ class TrainingPipeline:
             return data_ingestion_artifact
         except Exception as e:
             raise SensorException(e, sys)
+    
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            data_validation_config = DataValidationConfig(
+                training_pipeline_config=self.training_pipleine_config)
+            data_validation = DataValidation(data_validation_config=data_validation_config,
+             data_ingestion_artifact=data_ingestion_artifact)
+            
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise SensorException(e, sys)
 
 
 
@@ -50,8 +61,8 @@ class TrainingPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
 
-            # data_validation_artifact = self.start_data_validation(
-            #     data_ingestion_artifact=data_ingestion_artifact)
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact)
 
             # data_transformation_artifact = self.start_data_transformation(
             #     data_validation_artifact=data_validation_artifact
--- sensor/utils.py
+++ sensor/utils.py
@@ -3,8 +3,8 @@ import pandas as pd
 from .exception import SensorException
 import os,sys
 import json
-# import yaml
-# import dill
+import yaml
+import dill
 import numpy as np
 import logging
 def dump_csv_file_to_mongodb_collection(file_path:str,database_name:str,collection_name:str)->None:
@@ -29,22 +29,22 @@ def export_collection_as_dataframe(database_name:str,collection_name:str)->pd.Da
         raise SensorException(e,sys)
 
 
-# def write_yaml_file(file_path,data:dict):
-#     try:
-#         file_dir = os.path.dirname(file_path)
-#         os.makedirs(file_dir,exist_ok=True)
-#         with open(file_path,"w") as file_writer:
-#             yaml.dump(data,file_writer)
-#     except Exception as e:
-#         raise SensorException(e, sys)
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise SensorException(e, sys)
 
 
-# def read_yaml_file(file_path):
-#     try:
-#         with open(file_path,"rb") as file_reader:
-#             return yaml.safe_load(file_reader)
-#     except Exception as e:
-#         raise SensorException(e, sys)
+def read_yaml_file(file_path):
+    try:
+        with open(file_path,"rb") as file_reader:
+            return yaml.safe_load(file_reader)
+    except Exception as e:
+        raise SensorException(e, sys)
 
 
 
