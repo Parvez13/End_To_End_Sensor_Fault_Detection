4a4f37682388daefd0d2cc59ee9ac02662cdab0a
Parvez13 <prvzsohail@gmail.com>
2023-07-20 10:56:19 +0530

    Done pipeline completion from Data Transformation to Model Pusher components

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -40,3 +40,56 @@ class DataValidationConfig:
             self.missing_threshold = 70
         except Exception as e:
             raise SensorException(e, sys)
+
+class DataTransformationConfig:
+    def __init__(self, training_pipeline_config:TrainingPipelineConfig):
+        try:
+            data_transformation_dir = os.path.join(training_pipeline_config.artifact_dir,"data_transformation")
+            self.transform_obj_dir = os.path.join(data_transformation_dir,"transformer")
+            self.transform_object_path = os.path.join(self.transform_obj_dir,TRANSFORMER_OBJECT_FILE_NAME)
+            self.transform_data = os.path.join(data_transformation_dir,"transform_data")
+            self.transform_train_path = os.path.join(self.transform_data,TRAIN_FILE_NAME.replace("csv","npz"))
+            self.transform_test_path = os.path.join(self.transform_data,TEST_FILE_NAME.replace("csv","npz"))
+            self.target_encoder_path = os.path.join(data_transformation_dir,"target_encoder",TARGET_ENCODER_OBJECT_FILE_NAME)
+            self.schema_file_path=os.path.join("schema.yaml")
+        except Exception as e:
+            raise SensorException(e, sys)
+
+class ModelTrainerConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        try:
+            model_trainer_dir = os.path.join(training_pipeline_config.artifact_dir , "model_trainer")
+            self.model_path = os.path.join(model_trainer_dir,"model",MODEL_FILE_NAME)
+            self.expected_score = 0.7
+            self.overfitting_threshold = 0.1
+        except Exception as e:
+            raise SensorException(e, sys)
+
+class ModelEvaluationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.change_threshold = 0.01
+        self.schema_file_path=os.path.join("schema.yaml")
+
+
+class ModelPusherConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_pusher_dir = os.path.join(training_pipeline_config.artifact_dir , "model_pusher")
+        self.saved_model_dir = os.path.join("saved_models")
+        self.pusher_model_dir = os.path.join(self.model_pusher_dir,"saved_models")
+        self.pusher_model_path = os.path.join(self.pusher_model_dir,MODEL_FILE_NAME)
+        self.pusher_transformer_path = os.path.join(self.pusher_model_dir,TRANSFORMER_OBJECT_FILE_NAME)
+        self.pusher_target_encoder_path = os.path.join(self.pusher_model_dir,TARGET_ENCODER_OBJECT_FILE_NAME)
+
+
+class BatchPredictionConfig:
+
+    def __init__(self):
+        try:
+            self.inbox_dir = os.path.join("data","inbox")
+            self.outbox_dir = os.path.join("data","outbox")
+            self.archive_dir = os.path.join("data","archive")
+            os.makedirs(self.outbox_dir ,exist_ok=True)
+            os.makedirs(self.archive_dir,exist_ok=True)
+        except Exception as e:
+            raise SensorException(e, sys)
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,11 +1,11 @@
 from sensor.entity.config_entity import (TrainingPipelineConfig,
                                         DataIngestionConfig,
-                                        DataValidationConfig)
-                                        # DataTransformationConfig,
-                                        # ModelTrainerConfig,
-                                        # ModelEvaluationConfig,
-                                        # ModelPusherConfig
-                                        # )
+                                        DataValidationConfig,
+                                        DataTransformationConfig,
+                                        ModelTrainerConfig,
+                                        ModelEvaluationConfig,
+                                        ModelPusherConfig
+                                        )
 from sensor.entity.artifact_entity import (DataIngestionArtifact,
                                         DataValidationArtifact,
                                         DataTransformationArtifact,
@@ -18,10 +18,10 @@ from sensor.logger import logging
 import os,sys
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
-# from sensor.components.data_transformation import DataTransformation
-# from sensor.components.model_trainer import ModelTrainer
-# from sensor.components.model_evaluation import ModelEvaluation
-# from sensor.components.model_pusher import ModelPusher
+from sensor.components.data_transformation import DataTransformation
+from sensor.components.model_trainer import ModelTrainer
+from sensor.components.model_evaluation import ModelEvaluation
+from sensor.components.model_pusher import ModelPusher
 
 class TrainingPipeline:
 
@@ -54,6 +54,53 @@ class TrainingPipeline:
             return data_validation.initiate_data_validation()
         except Exception as e:
             raise SensorException(e, sys)
+    
+    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact)->DataTransformationArtifact:
+        try:
+            data_transformation_config = DataTransformationConfig(
+                training_pipeline_config=self.training_pipleine_config)
+            data_transformation = DataTransformation(data_transformation_config=data_transformation_config,
+             data_validation_artifact=data_validation_artifact)
+            
+            return data_transformation.initiate_data_transformation()
+        except Exception as e:
+            raise SensorException(e, sys)
+    
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
+        try:
+            model_trainer_config = ModelTrainerConfig(
+                training_pipeline_config=self.training_pipleine_config)
+            model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,
+             data_transformation_artifact=data_transformation_artifact)
+            
+            return model_trainer.initiate_model_trainer()
+        except Exception as e:
+            raise SensorException(e, sys)
+    
+    def start_model_evaluation(self,   data_validation_artifact: DataValidationArtifact,
+        data_transformation_artifact:DataTransformationArtifact,
+        model_trainer_artifact:ModelTrainerArtifact)->ModelEvaluationArtifact:
+        try:
+            model_eval_config = ModelEvaluationConfig(training_pipeline_config=self.training_pipleine_config)
+            model_eval = ModelEvaluation(model_eval_config=model_eval_config,
+             data_validation_artifact=data_validation_artifact,
+             data_transformation_artifact=data_transformation_artifact,
+             model_trainer_artifact=model_trainer_artifact)
+            return model_eval.initiate_model_evaluation()
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    def start_model_pusher(self,  data_transformation_artifact:DataTransformationArtifact,
+        model_trainer_artifact:ModelTrainerArtifact)->ModelPusherArtifact:
+        try:
+            model_pusher_config = ModelPusherConfig(training_pipeline_config=self.training_pipleine_config)
+            model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
+             data_transformation_artifact=data_transformation_artifact, 
+             model_trainer_artifact=model_trainer_artifact)
+            return model_pusher.initiate_model_pusher()
+        except Exception as e:
+            raise SensorException(e, sys)
+
 
 
 
@@ -64,18 +111,18 @@ class TrainingPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact)
 
-            # data_transformation_artifact = self.start_data_transformation(
-            #     data_validation_artifact=data_validation_artifact
-            # )
+            data_transformation_artifact = self.start_data_transformation(
+                data_validation_artifact=data_validation_artifact
+            )
 
-            # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             
 
-            # model_eval_artifact = self.start_model_evaluation(data_validation_artifact=data_validation_artifact,
-            #                 data_transformation_artifact=data_transformation_artifact,
-            #                 model_trainer_artifact=model_trainer_artifact)
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact=data_validation_artifact,
+                            data_transformation_artifact=data_transformation_artifact,
+                            model_trainer_artifact=model_trainer_artifact)
 
-            # model_pusher_artifact = self.start_model_pusher(data_transformation_artifact=data_transformation_artifact,
-            #                 model_trainer_artifact=model_trainer_artifact)
+            model_pusher_artifact = self.start_model_pusher(data_transformation_artifact=data_transformation_artifact,
+                            model_trainer_artifact=model_trainer_artifact)
         except Exception as e:
             raise SensorException(e, sys)
